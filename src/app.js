cded816d27373613dc4b592ea85722440e030207
israel125x <30633437+israel125x@users.noreply.github.com>
2017-10-18 17:51:07 -0500

    logs 18101750
    logs

--- src/app.js
+++ src/app.js
@@ -321,6 +321,12 @@ class FacebookBot {
                 let responseData = response.result.fulfillment.data;
                 let responseMessages = response.result.fulfillment.messages;
 
+                console.log('doApiAiRequest response.result ', response.result);
+                //console.log('doApiAiRequest sender: ',sender);
+                //console.log('response.result.metadata.intentName: ',response.result.metadata.intentName);
+                //console.log('response.result.parameters.valor: ',response.result.parameters.valor);
+                //console.log('response.sessionId: ',response.sessionId);
+
                 if (this.isDefined(responseData) && this.isDefined(responseData.facebook)) {
                     let facebookResponseData = responseData.facebook;
                     this.doDataResponse(sender, facebookResponseData);
