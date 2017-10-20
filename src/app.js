b1dfa86db2abed695787db1170e7f5ee8a266bb1
israel125x <30633437+israel125x@users.noreply.github.com>
2017-10-20 17:21:07 -0500

    remmplazo zipcode
    zip code test 1

--- src/app.js
+++ src/app.js
@@ -606,11 +606,16 @@ app.post('/webhook/', (req, res) => {
                                     geocoder.reverse({ lat: event.message.attachments[0].payload.coordinates.lat, lon: event.message.attachments[0].payload.coordinates.long.toString() })
                                         .then(function(res) {
                                             console.log('zipcode: ', res[0].zipcode);
+                                            // delete all locations from original message
+                                            event.message.attachments = event.message.attachments.filter(a => a.type !== "location");
+                                            // se añade codigo postal como texto del mesaje 
+                                            event.text = res[0].zipcode;
+                                            facebookBot.processMessageEvent(event);
                                         })
                                         .catch(function(err) {
                                             console.log(err);
                                         });
-                                    return null;
+                                    //return null;
                                     //fin geolocalizacion inversa 
                                     locations.forEach(l => {
                                         let locationEvent = {
