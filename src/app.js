0ba067412c5e2c806f6190698c54dd448abd4f5a
israel125x <30633437+israel125x@users.noreply.github.com>
2017-10-25 16:18:50 -0500

    test lista 2

--- src/app.js
+++ src/app.js
@@ -98,29 +98,62 @@ function solicitudReto() {
     var ref = db.ref("produccion/usuarios/facebook/");
     var count = 0;
     let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "list",
-                "top_element_style": "compact",
-                "elements": [{
-                    "title": "deseas participar en una encusta ?",
-                    "subtitle": "obten recompenzas ",
+        "type": "template",
+        "payload": {
+            "template_type": "list",
+            "top_element_style": "compact",
+            "elements": [{
+                    "title": "Classic T-Shirt Collection",
+                    "subtitle": "See all our colors",
+                    "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",
                     "buttons": [{
-                            "type": "postback",
-                            "title": "SI deseo participar ",
-                            "payload": "Camp1"
-                        },
-                        {
-                            "type": "postback",
-                            "title": "NO deseo participar ",
-                            "payload": "Nada"
-                        }
-                    ]
-                }]
-            }
+                        "title": "View",
+                        "type": "web_url",
+                        "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                        "messenger_extensions": true,
+                        "webview_height_ratio": "tall",
+                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                    }]
+                },
+                {
+                    "title": "Classic White T-Shirt",
+                    "subtitle": "See all our colors",
+                    "default_action": {
+                        "type": "web_url",
+                        "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                        "messenger_extensions": true,
+                        "webview_height_ratio": "tall",
+                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                    }
+                },
+                {
+                    "title": "Classic Blue T-Shirt",
+                    "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+                    "subtitle": "100% Cotton, 200% Comfortable",
+                    "default_action": {
+                        "type": "web_url",
+                        "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                        "messenger_extensions": true,
+                        "webview_height_ratio": "tall",
+                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                    },
+                    "buttons": [{
+                        "title": "Shop Now",
+                        "type": "web_url",
+                        "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                        "messenger_extensions": true,
+                        "webview_height_ratio": "tall",
+                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                    }]
+                }
+            ],
+            "buttons": [{
+                "title": "View More",
+                "type": "postback",
+                "payload": "payload"
+            }]
         }
-    }
+    };
 
     function asyncSqrt(ref, callback) {
         try {
