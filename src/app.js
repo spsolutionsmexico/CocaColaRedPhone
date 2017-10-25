696d0c4634a0d174b1f73f212acdebe0bf219e9a
israel125x <30633437+israel125x@users.noreply.github.com>
2017-10-25 16:49:34 -0500

    test lis 2

--- src/app.js
+++ src/app.js
@@ -104,58 +104,39 @@ function solicitudReto() {
                 "template_type": "list",
                 "top_element_style": "compact",
                 "elements": [{
-                        "title": "Classic T-Shirt Collection",
-                        "subtitle": "See all our colors",
-                        "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",
+                        "title": "deseas participar en una encusta ?",
+                        "subtitle": "obten recompenzas ",
                         "buttons": [{
-                            "title": "View",
-                            "type": "web_url",
-                            "url": "https://peterssendreceiveapp.ngrok.io/collection",
-                            "messenger_extensions": true,
-                            "webview_height_ratio": "tall",
-                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                        }]
+                                "type": "postback",
+                                "title": "SI deseo participar ",
+                                "payload": "Camp1"
+                            },
+                            {
+                                "type": "postback",
+                                "title": "NO deseo participar ",
+                                "payload": "Nada"
+                            }
+                        ]
                     },
                     {
-                        "title": "Classic White T-Shirt",
-                        "subtitle": "See all our colors",
-                        "default_action": {
-                            "type": "web_url",
-                            "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
-                            "messenger_extensions": true,
-                            "webview_height_ratio": "tall",
-                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                        }
-                    },
-                    {
-                        "title": "Classic Blue T-Shirt",
-                        "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
-                        "subtitle": "100% Cotton, 200% Comfortable",
-                        "default_action": {
-                            "type": "web_url",
-                            "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
-                            "messenger_extensions": true,
-                            "webview_height_ratio": "tall",
-                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                        },
+                        "title": "deseas participar en una encusta ?",
+                        "subtitle": "obten recompenzas ",
                         "buttons": [{
-                            "title": "Shop Now",
-                            "type": "web_url",
-                            "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
-                            "messenger_extensions": true,
-                            "webview_height_ratio": "tall",
-                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                        }]
+                                "type": "postback",
+                                "title": "SI deseo participar ",
+                                "payload": "Camp1"
+                            },
+                            {
+                                "type": "postback",
+                                "title": "NO deseo participar ",
+                                "payload": "Nada"
+                            }
+                        ]
                     }
-                ],
-                "buttons": [{
-                    "title": "View More",
-                    "type": "postback",
-                    "payload": "payload"
-                }]
+                ]
             }
         }
-    };
+    }
 
     function asyncSqrt(ref, callback) {
         try {
