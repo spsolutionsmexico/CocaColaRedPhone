d3b742361789b4fde693f6458fb5f82309cac3e2
israel125x <30633437+israel125x@users.noreply.github.com>
2017-11-21 17:20:17 -0600

    logs

--- src/app.js
+++ src/app.js
@@ -609,6 +609,7 @@ class FacebookBot {
                     if (value.lifespan == 1) {
                         console.log('doApiAiRequest sender: ', sender);
                         console.log('response.result.parameters.valor: ', response.result.parameters.valor);
+                        console.log('response.result.parameters.valor.original: ', response.result.parameters.valor.original);
                         console.log('contexto: ', value.name);
                         //grabar el fin del proceso de alta 
                         if (value.name === 'alta-fin') {
@@ -627,7 +628,7 @@ class FacebookBot {
                     }
                     if (value.lifespan == 2) {
                         console.log('LS=2 value.name: ', value.name);
-                        console.log('LS=2 response.result.parameters: ', response.result.parameters);
+                        console.log('LS=2 response.result.parameters.valor.original: ', response.result.parameters.valor.original);
                     }
                     console.log('value.name.indexOf -fin:', value.name.indexOf('-fin'));
                     if (value.name.indexOf('-fin') > 0 && value.name != 'alta-fin') {
